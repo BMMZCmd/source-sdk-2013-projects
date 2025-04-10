420b8fd6a58fa0b269e213fc539eb9ba6280d64f
BMMZCmd <bhonemyintmyantzaw@gmail.com>
2025-04-10 08:00:54 +0630

    yes, again

--- sp/src/game/server/dss/weapons/weapon_m60.cpp
+++ sp/src/game/server/dss/weapons/weapon_m60.cpp
@@ -13,6 +13,12 @@ public:
 
 	DECLARE_SERVERCLASS();
 
+	virtual void	ItemPostFrame(void);
+	virtual void	FireBullets(const FireBulletsInfo_t& info);
+	virtual float	GetFireRate(void) { return; };
+	virtual int		WeaponRangeAttack1Condition(float flDot, float flDist);
+	virtual bool	Deploy(void);
+
 	void AddViewKick(void);
 
 	int CapabilitiesGet(void) { return bits_CAP_WEAPON_RANGE_ATTACK1; }
@@ -22,16 +28,42 @@ public:
 		static const Vector cone = VECTOR_CONE_5DEGREES;
 		return cone;
 	}
-
-	DECLARE_ACTTABLE();
 };
+
+LINK_ENTITY_TO_CLASS(weapon_m60, CWeaponM60);
+PRECACHE_WEAPON_REGISTER(weapon_m60);
+
+IMPLEMENT_SERVERCLASS_ST(CWeaponM60, DT_WeaponM60)
+END_SEND_TABLE()
+
+BEGIN_DATADESC(CWeaponM60)
+END_DATADESC()
 
 CWeaponM60::CWeaponM60() 
 {
-
 }
 
-void CWeaponM60::AddViewKick()
+void CWeaponM60::ItemPostFrame(void)
+{
+	BaseClass::ItemPostFrame();
+}
+
+void CWeaponM60::FireBullets(const FireBulletsInfo_t& info)
+{
+	BaseClass::FireBullets(info);
+}
+
+int CWeaponM60::WeaponRangeAttack1Condition(float flDot, float flDist)
+{
+	return BaseClass::WeaponRangeAttack1Condition(flDot, flDist);
+}
+
+bool CWeaponM60::Deploy(void)
+{
+	return BaseClass::Deploy();
+}
+
+void CWeaponM60::AddViewKick(void)
 {
 	float flEasyDampen = 0.1f;
 	float flMaxVerticalKick = 20.0f;	//Degrees
